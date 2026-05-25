713afb6c4402d860d4fe95ae14872bb47e939b56
abhivadiyala26 <abhivadiyala@gmail.com>
2026-05-25 14:20:57 +0530

    remove package lock

--- backend/src/main/java/com/abhi/urlshortener/UrlshortenerApplication.java
+++ backend/src/main/java/com/abhi/urlshortener/UrlshortenerApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UrlshortenerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(UrlshortenerApplication.class, args);
+
+        System.out.println("\n=================================");
+        System.out.println("Application Running Successfully");
+        System.out.println("Open in Browser:");
+        System.out.println("http://localhost:8080");
+        System.out.println("=================================\n");
     }
 }
