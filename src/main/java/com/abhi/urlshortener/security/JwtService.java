fdefa79bbad8cc127d59e82bebecb9a09266ded6
abhivadiyala26 <abhivadiyala@gmail.com>
2026-05-30 20:34:31 +0530

    Fix JWT key

--- src/main/java/com/abhi/urlshortener/security/JwtService.java
+++ src/main/java/com/abhi/urlshortener/security/JwtService.java
@@ -75,7 +75,8 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
+        return Keys.hmacShaKeyFor(
+                secretKey.getBytes(java.nio.charset.StandardCharsets.UTF_8)
+        );
     }
 }
